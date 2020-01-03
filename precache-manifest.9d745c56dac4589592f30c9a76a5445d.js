8f7abf869d0b6ceea6003a111779a1a2f28bc72c
sarL3y <matt.tapper@gmail.com>
2020-01-03 00:14:42 -0800

    Updates

---
+++ precache-manifest.9d745c56dac4589592f30c9a76a5445d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2bd2d2ecbfe92cd344c12ec9835a019",
+    "url": "/mwfc/index.html"
+  },
+  {
+    "revision": "2b3fa69b8e4b65a893d4",
+    "url": "/mwfc/static/css/main.17f00fb4.chunk.css"
+  },
+  {
+    "revision": "abfbec0f7bd3cd2cafb9",
+    "url": "/mwfc/static/js/2.5188caab.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mwfc/static/js/2.5188caab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b3fa69b8e4b65a893d4",
+    "url": "/mwfc/static/js/main.3d2136be.chunk.js"
+  },
+  {
+    "revision": "47fe198ecc52181e589a",
+    "url": "/mwfc/static/js/runtime-main.2285a5d3.js"
+  }
+]);
